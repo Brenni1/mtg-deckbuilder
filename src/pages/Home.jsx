31d6e4339d1a0ed148ebd71437e608600e6c0896
bendfriedman <159451716+bendfriedman@users.noreply.github.com>
2024-04-02 23:57:38 +0200

    added some first structure and styling to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,30 @@
+import banner from "../assets/imgs/mtg-banner.jpg";
+
 export const Home = () => {
   return (
     <>
       <div className="home-banner">
-        <form>
-          <label>
-            <input
-              className="search-bar"
-              type="text"
-              placeholder="Search for Cards..."
-            />
-          </label>
-        </form>
-        <div>
-          <button>+New Deck+</button>
-          <button>Deck Folder</button>
+        <div
+          className="banner-background-img"
+          style={{ backgroundImage: "url(" + banner + ")" }}
+        ></div>
+        <div className="banner-fade fade-top"></div>
+        <div className="banner-form">
+          <form>
+            <label>
+              <input
+                className="banner-search-bar"
+                type="text"
+                placeholder="Search for Cards..."
+              />
+            </label>
+          </form>
+          <div className="banner-btns">
+            <button>+New Deck+</button>
+            <button>Deck Folder</button>
+          </div>
         </div>
+        <div className="banner-fade fade-bottom"></div>
       </div>
 
       <div className="deck-area">
